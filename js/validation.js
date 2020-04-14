9a06779d8018b715816d8341690da0c99a95fc86
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-04-14 13:56:03 -0700

    can recieve valid emails

--- js/validation.js
+++ js/validation.js
@@ -1,13 +1,27 @@
 // Can only contain letters a-z in lowercase and can be one or two names **first and last names
-function isValidUsername(username) {
-    return userNameRegex.test(username);
-  }
+const isValidUsername = (userName) => {
+    return userNameRegex.test(userName)
+}
 
 // Must be a valid email address, too be more specific it has to have a @ symbol a . and either a com,org etc...
-function isValidEmail(email) {
-    return emailRegex;
+const isValidEmail = (userEmail) => {
+    return emailRegex.test(userEmail)
 }
 
-const submitButtonClicked = () => {
-    console.log("submitbutton clicked");
+//this function brings everything together
+const submitButtonClicked = (event) => {
+    //at the moment I want to prevent defualt behavior
+    event.preventDefault();
+    if (isValidUsername(usersName.value)){
+        console.log("username is valid")
+    } else {
+        console.log("username is not valid")
+    }
+    if (isValidEmail(usersEmail.value)){
+        console.log("user email is valid")
+    } else {
+        console.log("user email is not valid")
+    }
 }
+
+submitButton.addEventListener('click', submitButtonClicked);
--- js/variables.js
+++ js/variables.js
@@ -1,8 +1,8 @@
 "use strict";
 //first off need to take in all of the fields that I will be getting data from
 
-const usersName = document.getElementById('user-name');
-const usersEmail = document.getElementById('user-email');
+const usersName = document.getElementById('name');
+const usersEmail = document.getElementById('mail');
 const usersJobRole = document.getElementById('user-email');
 const userSize = document.getElementById('user-size');
 const userDesign = document.getElementById('color');
@@ -20,7 +20,7 @@ const form = document.getElementById('form');
 
 //project regex
 
-const userNameRegex = /^\w|\d+$/;
+const userNameRegex = /^\w+$/;
 const emailRegex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
 
 
