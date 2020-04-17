63261190054597a0b6855f91ce898d08c8c16f70
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-04-16 22:49:23 -0700

    remove first t-shirt design option in select field

--- js/main.js
+++ js/main.js
@@ -5,3 +5,6 @@ usersName.focus()
 
 //hide the other input field as stated in the instructions
 otherInputField.style.display = "none";
+
+//I need to hide the first element in the Design selection box
+tshirtDesign.removeChild(tshirtDesign[0]);
