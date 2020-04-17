d63d4601541d84607542806c5ee54ff42c9bea45
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-04-16 19:15:06 -0700

    add regex grouping

--- js/validation.js
+++ js/validation.js
@@ -1,11 +1,26 @@
 // Can only contain letters a-z in lowercase and can be one or two names **first and last names
-const isValidUsername = (userName) => {
-    return userNameRegex.test(userName)
+const isValidUsername = (userNameValue) => {
+    return userNameRegex.test(userNameValue)
 }
 
 // Must be a valid email address, too be more specific it has to have a @ symbol a . and either a com,org etc...
-const isValidEmail = (userEmail) => {
-    return emailRegex.test(userEmail)
+const isValidEmail = (userEmailValue) => {
+    return emailRegex.test(userEmailValue)
+}
+
+// Must be a valid credit card number, digit groups of four
+const isValidCreditCardNumber = (userCreditCardNumberValue) => {
+    return creditCardRegex.test(userCreditCardNumberValue)
+}
+
+// Must be a valid credit card number, digit groups of four
+const isValidZipcode = (userZipCode) => {
+    return zipCodeRegex.test(userZipCode)
+}
+
+// Must be a valid credit card number, digit groups of four
+const isValidCvvCode = (cvvCode) => {
+    return cvvCodeRegex.test(cvvCode)
 }
 
 //this function brings everything together
@@ -21,6 +36,21 @@ const submitButtonClicked = (event) => {
         console.log("user email is valid")
     } else {
         console.log("user email is not valid")
+    }
+    if (isValidCreditCardNumber(userCreditCardNumber.value)){
+        console.log("user credit card is valid")
+    } else {
+        console.log("user credit card is not valid")
+    }
+    if (isValidZipcode(userZipCode.value)){
+        console.log("user zipcode is valid")
+    } else {
+        console.log("user zipcode is not valid")
+    }
+    if (isValidCvvCode(cardVerificationValue.value)){
+        console.log("user CVV is valid")
+    } else {
+        console.log("user CVV is not valid")
     }
 }
 
--- js/variables.js
+++ js/variables.js
@@ -8,9 +8,9 @@ const userSize = document.getElementById('user-size');
 const userDesign = document.getElementById('color');
 const userActivities = document.getElementsByClassName('.activities');
 const userPayment = document.getElementById('payment');
-const userCrediCardNumber = document.getElementById('cc-num');
+const userCreditCardNumber = document.getElementById('cc-num');
 const userZipCode = document.getElementById('zip');
-const userCv = document.getElementById('cvv');
+const cardVerificationValue = document.getElementById('cvv');
 const userExpMonth = document.getElementById('exp-month');
 const userExpYear = document.getElementById('exp-year');
 const submitButton = document.querySelector('button');
@@ -22,5 +22,8 @@ const form = document.getElementById('form');
 //this handled mutiple names and does not permit numbers
 const userNameRegex = /^[a-z ,.'-]+$/;
 const emailRegex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
-
+const creditCardRegex = /(\d{4})( |-|)(\d{4})( |-|)(\d{4})( |-|)(\d{4})/
+const creditCardReplace = ;
+const zipCodeRegex = /^\d{4}\d?$/
+const cvvCodeRegex = /\d{3}\d?/
 
