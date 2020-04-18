dbca8e72b66f008e2e456918f52330b806bd2ef0
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-04-17 22:25:31 -0700

    fix manipulateColorSelectFieldActive functionality inline & none

---
+++ js/defaults.js
@@ -0,0 +1,29 @@
+//hide the other input field as stated in the instructions
+//tshirtDesign.removeChild(tshirtDesign[0]);
+const otherJobInputFieldDisplay = (displayType) => {
+    otherInputField.style.display = displayType;
+}
+
+//this function handles the defualt and active settings and act
+const manipulateColorSelectFieldActive = (displayType) => {
+ //createing a default state for selection field
+ const defaultOption = document.createElement("option");
+ defaultOption.innerHTML = "Please select a T-shirt theme"
+ const firsChild = colorSelectField.childNodes[0];
+ let i = 1
+    //createing a default state for selection field
+    if(displayType === "none"){
+    colorSelectField.insertBefore(defaultOption, firsChild);
+    } else {
+    //I need to hide the first element in the Design selection box
+    i = 0
+
+    colorSelectField.removeChild(colorSelectField[0]);
+    }
+    colorSelectField.selectedIndex = 0;
+    //loop through all elements in this select field
+    while (i < colorSelectField.length) {
+        colorSelectField[i].style.display = displayType;
+        i++;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,30 +1,15 @@
 "use strict";
-    //createing a default state for selection field
-    const defaultOption = document.createElement("option");
-    defaultOption.innerHTML = "Please select a T-shirt theme"
-    const firsChild = colorSelectField.childNodes[0]
-    
-    //hide the other input field as stated in the instructions
-
-    
-const colorSelectFieldDefaults = () => {
-    //we need the user to focus on the first input field
-    usersName.focus()
-    //I need to hide the first element in the Design selection box
-    tshirtDesign.removeChild(tshirtDesign[0]);
-    colorSelectField.insertBefore(defaultOption, firsChild);
-    colorSelectField.selectedIndex = 0;
-    let i = 1;
-    //loop through all elements in this select field
-    while (i < colorSelectField.length) {
-        colorSelectField[i].style.display = "none";
-        i++;
-      }
-}    
-
+ 
 //I want to encapsulated these diffrent defaults being set
 const setDefaults = () => {
-    colorSelectFieldDefaults();
+    //we need the user to focus on the first input field
+    usersName.focus()
+    otherJobInputFieldDisplay("none");
+    manipulateColorSelectFieldActive("none")
+    manipulateColorSelectFieldActive("inline")
 }
+
 setDefaults();
+
+
 
